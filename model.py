8d8fadbd6cd5862852547b7ed15b8582e25c8382
Rafał Kaufman <kaufman.rafal@gmail.com>
2024-01-27 15:35:09 +0100

    Build encoder–decoder LSTM model #1

--- model.py
+++ model.py
@@ -1,4 +1,15 @@
 import pandas as pd
+from keras import backend as K
+from keras.layers import (
+    LSTM,
+    Attention,
+    Concatenate,
+    Dense,
+    Embedding,
+    Input,
+    TimeDistributed,
+)
+from keras.models import Model
 from keras.preprocessing.sequence import pad_sequences
 from keras.preprocessing.text import Tokenizer
 
@@ -49,3 +60,45 @@ y_validate = pad_sequences(y_validate, maxlen=max_len_summary, padding="post")
 
 y_voc_size = len(train_summary_tokenizer.word_index) + 1
 print(y_voc_size)
+# ------Encoder-----------------------------------------------------------------
+K.clear_session()
+word_vector_dim = 500
+
+encoder_inputs = Input(shape=(max_len_dialogue,))
+encoder_embedding = Embedding(x_voc_size, word_vector_dim, trainable=True)
+encoder_embedding_inputs = encoder_embedding(encoder_inputs)
+
+# LSTM 1
+encoder_lstm1 = LSTM(word_vector_dim, return_sequences=True, return_state=True)
+encoder_output1, state_h1, state_c1 = encoder_lstm1(encoder_embedding_inputs)
+
+# LSTM 2
+encoder_lstm2 = LSTM(word_vector_dim, return_sequences=True, return_state=True)
+encoder_output2, state_h2, state_c2 = encoder_lstm2(encoder_output1)
+
+# LSTM 3
+encoder_lstm3 = LSTM(word_vector_dim, return_state=True, return_sequences=True)
+encoder_outputs, state_h, state_c = encoder_lstm3(encoder_output2)
+# ------Decoder-----------------------------------------------------------------
+decoder_inputs = Input(shape=(None,))
+decoder_embedding = Embedding(y_voc_size, word_vector_dim, trainable=True)
+decoder_embedding_inputs = decoder_embedding(decoder_inputs)
+
+# LSTM using encoder_states as initial state
+decoder_lstm = LSTM(word_vector_dim, return_sequences=True, return_state=True)
+decoder_outputs, decoder_fwd_state, decoder_back_state = decoder_lstm(
+    decoder_embedding_inputs, initial_state=[state_h, state_c]
+)
+# ------Attention Layer-----------------------------------------------------------------
+attention = Attention()
+attention_outputs = attention([decoder_outputs, encoder_outputs])
+
+concatenate = Concatenate(axis=-1)
+decoder_concat_input = concatenate([decoder_outputs, attention_outputs])
+# ------Dense layer-----------------------------------------------------------------
+decoder_dense = TimeDistributed(Dense(y_voc_size, activation="softmax"))
+decoder_outputs = decoder_dense(decoder_concat_input)
+
+# Define the model
+model = Model([encoder_inputs, decoder_inputs], decoder_outputs)
+model.summary()
